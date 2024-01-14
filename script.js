c0f46d7ecc5ce6f13c056062711ed370a71db835
Sahil Sonvane <149565986+Sahil-Sonvane@users.noreply.github.com>
2024-01-14 20:47:23 +0530

    update index.html and script.js

--- script.js
+++ script.js
@@ -1,26 +1,218 @@
 const projects = [
-    {
-        name : "Weather App",
-        icon : "fa-solid fa-plus-minus",
-        image: "images/age-calculator.png",
-        url : "/30-plus-mini-js-projects/weather-app/",
-        code:"https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/age-calculator-app"
-    },
-    {
-        icon : "",
-        name : "Project 2",
-        url : ""
-    },
-    {
-        icon : "",
-        name : "Project 3",
-        url : ""
-    }
-]
-console.log(Links)
+  {
+    name: "Weather App",
+    icon: "fa-solid fa-temperature-low",
+    image: "/30-plus-mini-js-projects/images/age-calculator.png",
+    url: "/30-plus-mini-js-projects/weather-app/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/age-calculator-app",
+  },
+  {
+    name: "Age Calculator",
+    icon: "fa-solid fa-plus-minus",
+    image: "/30-plus-mini-js-projects/images/age-calculator.png",
+    url: "/30-plus-mini-js-projects/age-calculator/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/age-calculator-app",
+  },
+  {
+    name: "Calculator",
+    icon: "fa-solid fa-calculator",
+    image: "/30-plus-mini-js-projects/images/calculator.png",
+    url: "/30-plus-mini-js-projects/calculator",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/calculator",
+  },
+  {
+    name: "Circular Progress Bar",
+    icon: "fa-solid fa-circle-notch",
+    image: "/30-plus-mini-js-projects/images/circular-progressbar.png",
+    url: "/30-plus-mini-js-projects/circular-progress-bar/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/circular-progress-bar/",
+  },
+  {
+    name: "Coming Soon Page",
+    icon: "fa-solid fa-clock",
+    image: "/30-plus-mini-js-projects/images/coming-soon-page.png",
+    url: "/30-plus-mini-js-projects/coming-soon-page/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/coming-soon-page/",
+  },
+  {
+    name: "CryptoCurrency Price Webpage",
+    icon: "fa-brands fa-bitcoin",
+    image: "/30-plus-mini-js-projects/images/cryptocurrency.png",
+    url: "/30-plus-mini-js-projects/cryptocurrency-price-website/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/cryptocurrency-price-website/",
+  },
+  {
+    name: "Digital Clock",
+    icon: "fa-regular fa-clock",
+    image: "/30-plus-mini-js-projects/images/digital-clock.png",
+    url: "/30-plus-mini-js-projects/digital-clock/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/digital-clock/",
+  },
+  {
+    name: "Drag & Drop",
+    icon: "fa-solid fa-hand-pointer",
+    image: "/30-plus-mini-js-projects/images/drag-n-drop.png",
+    url: "/30-plus-mini-js-projects/drag-and-drop/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/drag-and-drop/",
+  },
+  {
+    name: "Form Validation",
+    icon: "fa-brands fa-wpforms",
+    image: "/30-plus-mini-js-projects/images/form-validation.png",
+    url: "/30-plus-mini-js-projects/form-validation/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/form-validation/",
+  },
+  {
+    name: "Background Remover Effect",
+    icon: "fa-solid fa-images",
+    image: "/30-plus-mini-js-projects/images/background-remove-effect.png",
+    url: "/30-plus-mini-js-projects/image-background-change/",
+    code: "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/image-background-change/",
+  },
+  {
+    name : "Image Search Engine",
+    icon : "fa-solid fa-image",
+    image : "/30-plus-mini-js-projects/images/image-search-engine.png",
+    url : "/30-plus-mini-js-projects/image-search-engine/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/image-search-engine/"
+},
+{
+    name : "Mini Calendar",
+    icon : "fa-solid fa-calendar-days",
+    image : "/30-plus-mini-js-projects/images/mini-calendar.png",
+    url : "/30-plus-mini-js-projects/mini-calendar/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/mini-calendar/"
+},
+{
+    name : "Music Player",
+    icon : "fa-solid fa-music",
+    image : "/30-plus-mini-js-projects/images/music-player.png",
+    url : "/30-plus-mini-js-projects/music-player-app/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/music-player-app/"
+},{
+    name : "Note App",
+    icon : "fa-solid fa-note-sticky",
+    image : "/30-plus-mini-js-projects/images/note-app.png",
+    url : "/30-plus-mini-js-projects/note-app/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/note-app/"
+},{
+    name : "Password Generator",
+    icon : "fa-solid fa-bolt",
+    image : "/30-plus-mini-js-projects/images/password-generator.png",
+    url : "/30-plus-mini-js-projects/password-generator/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/password-generator/"
+},{
+    name : "Password Strength",
+    icon : "fa-solid fa-lock",
+    image : "/30-plus-mini-js-projects/images/password-strength.png",
+    url : "/30-plus-mini-js-projects/password-strength/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/password-strength/"
+},{
+    name : "Photo Gallery",
+    icon : "fa-solid fa-photo-film",
+    image : "/30-plus-mini-js-projects/images/photo-gallery.png",
+    url : "/30-plus-mini-js-projects/photo-gallery/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/photo-gallery/"
+},{
+    name : "Popup Notification",
+    icon : "fa-solid fa-comment-dots",
+    image : "/30-plus-mini-js-projects/images/popup-notificaion.png",
+    url : "/30-plus-mini-js-projects/popup-notification/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/popup-notification/"
+},
+{
+    name : "Product Page",
+    icon : "fa-solid fa-newspaper",
+    image : "/30-plus-mini-js-projects/images/product-page.png",
+    url : "/30-plus-mini-js-projects/product-page/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/product-page/"
+},{
+    name : "Qr Code Generator",
+    icon : "fa-solid fa-qrcode",
+    image : "/30-plus-mini-js-projects/images/qr-code.png",
+    url : "/30-plus-mini-js-projects/qr-code-generator/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/qr-code-generator/"
+},{
+    name : "Quiz App",
+    icon : "fa-regular fa-lightbulb",
+    image : "/30-plus-mini-js-projects/images/quiz.png",
+    url : "/30-plus-mini-js-projects/quiz-app/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/quiz-app/"
+},{
+    name : "Quote Generator",
+    icon : "fa-solid fa-quote-left",
+    image : "/30-plus-mini-js-projects/images/quote.png",
+    url : "/30-plus-mini-js-projects/quote-generator/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/quote-generator/"
+},{
+    name : "Custom Select Menu",
+    icon : "fa-solid fa-bars",
+    image : "/30-plus-mini-js-projects/images/select-menu.png",
+    url : "/30-plus-mini-js-projects/select-menu-design/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/select-menu-design/"
+},{
+    name : "Show/Hide Password",
+    icon : "fa-regular fa-eye-slash",
+    image : "/30-plus-mini-js-projects/images/password-show.png",
+    url : "/30-plus-mini-js-projects/show-password/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/show-password/"
+},{
+    name : "Stop Watch",
+    icon : "fa-solid fa-stopwatch",
+    image : "/30-plus-mini-js-projects/images/stop-watch.png",
+    url : "/30-plus-mini-js-projects/stopwatch/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/"
+},
+{
+    name : "Email Subscription",
+    icon : "fa-solid fa-envelope-circle-check",
+    image : "/30-plus-mini-js-projects/images/email.png",
+    url : "/30-plus-mini-js-projects/subscription-email/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/subscription-email/"
+},
+{
+    name : "Text to Speech",
+    icon : "fa-solid fa-volume-low",
+    image : "/30-plus-mini-js-projects/images/text-to-speech.png",
+    url : "/30-plus-mini-js-projects/text-to-voice-converter/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/text-to-voice-converter/"
+},
+{
+    name : "To Do List",
+    icon : "fa-solid fa-list-check",
+    image : "/30-plus-mini-js-projects/images/to-do.png",
+    url : "/30-plus-mini-js-projects/to-do-list-app/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/to-do-list-app/"
+},
+{
+    name : "Tost Notification",
+    icon : "fa-solid fa-chart-bar",
+    image : "/30-plus-mini-js-projects/images/toast-notification.png",
+    url : "/30-plus-mini-js-projects/toast-notification/",
+    code : "https://github.com/Sahil-Sonvane/30-plus-mini-js-projects/tree/main/toast-notification/"
+},
 
 
-Links.forEach((data)=>{
-    console.log(data[2]);
-    
-})
+
+];
+
+let ProjecTile = document.getElementById("project-tile");
+
+projects.forEach((data) => {
+  ProjecTile.innerHTML += `<div class="card">
+  <div class="visual">
+    <img src="${data.image}" />
+  </div>
+  <div class="info">
+    <h3><i class="${data.icon}"></i> ${data.name}</h3>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil,
+      architecto.
+    </p>
+    </div>
+    <div class="buttons">
+      <a href="${data.code}" class="btn">Code</a>
+      <a href="${data.url}" class="btn">Live</a>
+  </div>
+</div> `
+});
